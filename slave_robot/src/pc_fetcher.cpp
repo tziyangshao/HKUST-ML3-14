0fc92246113892004cf9675f66c6648568f34030
Daniel Schneider <schneida@student.ethz.ch>
2015-04-28 21:17:46 +0800

    included time in ptcl header... not yet all working properly NEED SAME TIMESTAMP FOR POINTCLOUD KEYFRAMES POSE

--- slave_robot/src/pc_fetcher.cpp
+++ slave_robot/src/pc_fetcher.cpp
@@ -1,155 +1,85 @@
-#pragma once
-#undef Success
-
-#include "KeyFrameDisplay.h"
-
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include "ros/ros.h"
-// -----------------------------
-// for image
-// -----------------------------
-//#include <message_filters/subscriber.h>
-//#include <message_filters/time_synchronizer.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud_conversion.h>
+#include "pc_fetcher.h"
 
 using namespace std; 
 using namespace sensor_msgs;
 
+pc_fetcher::pc_fetcher(ros::NodeHandle nh) : _slaveRobot(nh)
+{
+//    _imSub = _slaveRobot.subscribe("/usb_cam_node/image_raw", 30, &pc_fetcher::imCallback, this);
+    _imSub = _slaveRobot.subscribe("/usb_cam_r2/image_raw", 30, &pc_fetcher::imCallback, this);
+    _kfSub = _slaveRobot.subscribe("/lsd_slam_r2/graph", 10, &pc_fetcher::kfCallback, this);
+    _pcSub = _slaveRobot.subscribe("/lsd_slam_r2/keyframes", 20, &pc_fetcher::pcCallback, this);
+    _imPub = _slaveRobot.advertise<sensor_msgs::Image>("/slave_robot2/keyFrame", 5);
+    _pcPub = _slaveRobot.advertise<sensor_msgs::PointCloud2>("/slave_robot2/pointcloud2", 5);
+}
+pc_fetcher::~pc_fetcher(){
+	//destructor to release the memory
+}
 
-// -----------------------------
-// for pointcloud recovery
-// -----------------------------
-//parameters of the camera
-float fx, fy, cx, cy, fxi, fyi, cxi, cyi;
-int height, width;
-InputPointDense* originalInput;
-// camera pose
-// may be updated by kf-graph.
-Sophus::Sim3f camToWorld;
-// -----------------------------
-// for keyframe 
-// -----------------------------
-bool keyframe_trigger=0;
-sensor_msgs::Image keyframe;
-sensor_msgs::PointCloud pointcloud;
-sensor_msgs::PointCloud2 cloud2;
+void pc_fetcher::pcCallback(slave_robot::keyframeMsgConstPtr msg){
 
-	
-void pc_Callback(slave_robot::keyframeMsgConstPtr msg){
-	geometry_msgs::Point32 tempbuffer;
-	int point_count=0;
-	fx = msg->fx;
-	fy = msg->fy; 
-	cx = msg->cx;
-	cy = msg->cy;
+    geometry_msgs::Point32 _tempbuffer;
+    _fx = msg->fx;
+    _fy = msg->fy; 
+    _cx = msg->cx;
+    _cy = msg->cy;
 
-	fxi = 1/fx;
-	fyi = 1/fy;
-	cxi = -cx / fx;
-	cyi = -cy / fy;
+    _fxi = 1/_fx;
+    _fyi = 1/_fy;
+    _cxi = -_cx / _fx;
+    _cyi = -_cy / _fy;
+  
+    _width = msg->width;
+    _height = msg->height;
+    //pointcloud.header.stamp = msg->time;
+    _originalInput = new InputPointDense[_width*_height];
+    memcpy(_originalInput, msg->pointcloud.data(), _width*_height*sizeof(InputPointDense));
+    memcpy(_camToWorld.data(), msg->camToWorld.data(), 7*sizeof(float));
 
-	width = msg->width;
-	height = msg->height;
-	//pointcloud.header.stamp = msg->time;
-	originalInput = new InputPointDense[width*height];
-	memcpy(originalInput, msg->pointcloud.data(), width*height*sizeof(InputPointDense));
-	memcpy(camToWorld.data(), msg->camToWorld.data(), 7*sizeof(float));
-	// this is the place where they converte uint8 into float, if you
-	// dun get it, there is a link, 
-        //http://answers.ros.org/question/72469/roscpp-convert-uint8-into-a-known-struct/
-	for(int y=1;y<height-1;y++){
-		for(int x=1;x<width-1;x++){
-			float depth = 1/originalInput[x+y*width].idepth;
-			float depth4 = depth*depth; depth4*= depth4;
-			if(originalInput[x+y*width].idepth<=0)
-				continue;					
-			Sophus::Vector3f pt = camToWorld * (Sophus::Vector3f((x*fxi + cxi), (y*fyi + cyi), 1.0) * depth);
-			tempbuffer.x=pt[0];
-			tempbuffer.y=pt[1];
-			tempbuffer.z=pt[2];
-			pointcloud.points.push_back(tempbuffer);
-			point_count++;
-			}
+    for(int y=1;y<_height-1;y++)
+	for(int x=1;x<_width-1;x++){
+	    if(_originalInput[x+y*_width].idepth <= 0) continue;
+		float depth = 1 / _originalInput[x+y*_width].idepth;
+		float depth4 = depth*depth; depth4*= depth4;
+		if(_originalInput[x+y*_width].idepth_var * depth4 > scaledDepthVarTH) continue;
+		if(_originalInput[x+y*_width].idepth_var * depth4 * _camToWorld.scale()*_camToWorld.scale() > scaledDepthVarTH) continue;
+		if(minNearSupport > 1){
+		    int nearSupport = 0;
+			for(int dx=-1;dx<2;dx++)
+    			    for(int dy=-1;dy<2;dy++){
+				int idx = x+dx+(y+dy)*_width;
+				if(_originalInput[idx].idepth > 0){
+				    float diff = _originalInput[idx].idepth - 1.0f / depth;
+				    if(diff*diff < 2*_originalInput[x+y*_width].idepth_var)
+					nearSupport++;
+				}
+			    }
+		    if(nearSupport < minNearSupport)continue;
 		}
-	cout<< point_count <<endl;
-	sensor_msgs::convertPointCloudToPointCloud2(pointcloud,cloud2);	
-	ROS_INFO("pc calling back...");
+	    //Sophus::Vector3f pt = _camToWorld * (Sophus::Vector3f((x*_fxi + _cxi), (y*_fyi + _cyi), 1.0) * depth);
+	Sophus::Vector3f pt = (Sophus::Vector3f((x*_fxi + _cxi), (y*_fyi + _cyi), 1.0) * depth);
+	    _tempbuffer.x=pt[0];
+	    _tempbuffer.y=pt[1];
+            _tempbuffer.z=pt[2];
+	    _pointcloud.points.push_back(_tempbuffer);
+	}
+
+    sensor_msgs::convertPointCloudToPointCloud2(_pointcloud,_cloud2);	
+    _cloud2.header.seq=kfIndex;
+
+
+    ROS_INFO("pc calling back...");
+	ros::Time timeStamp(msg->time);
+	_cloud2.header.stamp = timeStamp;
+    _keyframe.header.seq=kfIndex;
+
+    _imPub.publish(_keyframe);
+    _pcPub.publish(_cloud2);
 }
 
-void keyframecount_Callback(slave_robot::keyframeGraphMsgConstPtr graph_msg){
-	pointcloud.header.seq= graph_msg-> numFrames;
-	pointcloud.header.frame_id = "robot";
-	keyframe_trigger=1;
-	ROS_INFO("keyframe calling back...");
+void pc_fetcher::kfCallback(slave_robot::keyframeGraphMsgConstPtr graph_msg){
+	kfIndex=(uint32_t)graph_msg-> numFrames;
 }
-
-void image_Callback(const ImageConstPtr& image){
-	keyframe = *image;
-}
-
-int main(int argc, char **argv){
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listening_keyframe");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle slave_robot;
-
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber pc_sub = slave_robot.subscribe("/lsd_slam/keyframes", 10, pc_Callback);
-  ros::Subscriber keyframecount_sub = slave_robot.subscribe("/lsd_slam/graph", 10, keyframecount_Callback);
-  ros::Subscriber image_sub = slave_robot.subscribe("/usb_cam_node/image_raw", 10, image_Callback);
-  ros::Publisher image_pub = slave_robot.advertise<sensor_msgs::Image>("/slave_robot/keyFrame", 5);
-  ros::Publisher pc_pub = slave_robot.advertise<sensor_msgs::PointCloud2>("/slave_robot/pointcloud", 5);
-  while (ros::ok()){
-    if(keyframe_trigger==1){
-      image_pub.publish(keyframe);
-      pc_pub.publish(cloud2);
-      keyframe_trigger=0;
-    }
-    ros::spinOnce();
-/*
-    if (pointcloud.points.size() >0){
-    cout<< pointcloud.points.size() <<endl;
-}
-*/
-    pointcloud.points.clear();
-}
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  return 0;
+void pc_fetcher::imCallback(const ImageConstPtr& image){
+	_keyframe = *image;
 }
