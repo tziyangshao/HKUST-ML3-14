6172333efa04c711af7335a68716823d6e7055a0
ShaoTziYang <tziyangshao@gmail.com>
2015-05-04 12:45:28 +0800

    modifying script

--- slave_robot/toDaniel.sh
+++ slave_robot/toDaniel.sh
@@ -6,6 +6,6 @@ rm change/Z_fetcher
 mv CMakeLists.txt change/Z_cmake
 mv src/pc_fetcher.cpp change/Z_fetcher
 
-mv change/D_cmake CMakeLists.txt
-mv change/D_fetcher src/pc_fetcher.cpp
+cp change/D_cmake CMakeLists.txt
+cp change/D_fetcher src/pc_fetcher.cpp
 
--- slave_robot/toZyan.sh
+++ slave_robot/toZyan.sh
@@ -6,6 +6,6 @@ rm change/D_fetcher
 mv CMakeLists.txt change/Z_cmake
 mv src/pc_fetcher.cpp change/Z_fetcher
 
-mv change/Z_cmake CMakeLists.txt
-mv change/Z_fetcher src/pc_fetcher.cpp
+cp change/Z_cmake CMakeLists.txt
+cp change/Z_fetcher src/pc_fetcher.cpp
 
